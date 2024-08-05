e279e75ce2aaeea328c3432edc37f40692ba9f79
Mulih <almulih@yahoo.com>
2024-08-05 16:31:17 +0300

    upload of project tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Lists documents. """
+
+
+def list_all(mongo_collection):
+    """ Lists documents in a collection."""
+    return [doc for doc in mongo_collection.fin()]
